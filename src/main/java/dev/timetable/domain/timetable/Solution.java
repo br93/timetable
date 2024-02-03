f4b6e1701fa2b63632a79f0f71369556747e66c4
br93 <reisalvesb@gmail.com>
2024-02-02 23:56:43 -0300

    refactor: remove field Lesson from Solution response json

--- src/main/java/dev/timetable/domain/timetable/Solution.java
+++ src/main/java/dev/timetable/domain/timetable/Solution.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Document(collection = "solutions")
+@JsonIgnoreProperties( { "lessons" })
 public class Solution {
 
     @MongoId(FieldType.OBJECT_ID)
